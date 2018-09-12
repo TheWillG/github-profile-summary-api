c345162addf3b6b958671e372f53e2f8db1f6989
Will Garcia <wjgarcia2013@gmail.com>
2018-09-12 09:00:22 -0400

    add user events to userData object (#19)
    Add user events

--- src/controllers/githubController.js
+++ src/controllers/githubController.js
@@ -5,7 +5,7 @@ const getUserData = async (req, res) => {
   const { userName } = req.params;
   try {
     const userData = await getData(userName);
-    res.status(200).send(userData.data.user);
+    res.status(200).send(userData);
   } catch (e) {
     logger.error(`Failed to get github user data with userName ${userName}`, e);
     res.status(500).send(e);
--- src/services/githubService.js
+++ src/services/githubService.js
@@ -1,9 +1,10 @@
 const fetch = require("cross-fetch");
 const gql = require("graphql-tag");
+const request = require("request-promise");
 const { ApolloClient } = require("apollo-boost");
 const { HttpLink } = require("apollo-link-http");
 const { InMemoryCache } = require("apollo-cache-inmemory");
-const { githubUserAccessToken } = require("../../lib/config");
+const { githubUserAccessToken, logger, eventTypes } = require("../../lib/config");
 
 const client = new ApolloClient({
   link: new HttpLink({
@@ -17,8 +18,23 @@ const client = new ApolloClient({
 });
 
 const getUserData = async userName => {
-  return client.query({
+
+  const requestOptions = {
+    uri: `https://api.github.com/users/${userName}/events/public`,
+    headers: {
+      authorization: `Bearer ${githubUserAccessToken}`,
+      "User-Agent": "TheWillG"
+    },
+    json: true
+  };
+
+  // Run both API requests in parallel
+  const eventsPromise = request(requestOptions);
+  const graphQlResponsePromise = client.query({
     query: gql`{
+            rateLimit {
+              remaining
+            }
             user(login: ${userName}) {
                 id
                 login
@@ -70,6 +86,38 @@ const getUserData = async userName => {
             }
         }`
   });
+
+  // Wait for all async calls to return
+  const graphQlResponse = await graphQlResponsePromise;
+  const events = await eventsPromise;
+
+  const userData = Object.assign({
+    ...graphQlResponse.data.user
+  }, {
+    events: formatEvents(events)
+  });
+  logger.info(`User Requested: ${userName}`);
+  logger.info(`Remaining Limit: ${graphQlResponse.data.rateLimit.remaining}`);
+
+  return userData;
+};
+
+const formatEvents = events => {
+  return events.filter(event => eventTypes.includes(event.type))
+          .map(({ type, repo, payload, created_at }) => {
+            let obj = {
+              type,
+              repo,
+              action: payload.action,
+              ref_type: payload.ref_type,
+              created_at
+            }
+            if (payload.pull_request) {
+              obj = Object.assign(obj, { merged: payload.pull_request.merged_at != null });
+            }
+            return obj;
+          })
+          .slice(0, 10);
 };
 
 module.exports = getUserData;
