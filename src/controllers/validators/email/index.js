9b058a65b20d9731973945b150b69d6aa998ebf7
TheWillG <wjgarcia2013@gmail.com>
2018-10-03 18:27:55 -0400

    refactor email validation to leverage Joi

--- src/controllers/validators/email/index.js
+++ src/controllers/validators/email/index.js
@@ -1,10 +1,9 @@
 const { celebrate, Joi } = require("celebrate");
-const emailValidationRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 const postEmailData = celebrate({
   body: {
-    senderEmail: Joi.string().required().regex(emailValidationRegex),
-    recipientEmail: Joi.string().required().regex(emailValidationRegex),
+    senderEmail: Joi.string().email().required(),
+    recipientEmail: Joi.string().email().required(),
     subject: Joi.string().required(),
     message: Joi.string().required()
   },
