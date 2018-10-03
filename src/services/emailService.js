e182bb17d44f41dfcb2914254eb45c265f17c76a
TheWillG <wjgarcia2013@gmail.com>
2018-10-03 18:17:04 -0400

    change email text to html

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,27 +1,29 @@
-const mailgunJS = require('mailgun-js');
-const { mailgunApiKey, mailgunDomain } = require('../../lib/config');
+const mailgunJS = require("mailgun-js");
+const { mailgunApiKey, mailgunDomain } = require("../../lib/config");
 
 const mailgunJSClient = mailgunJS({ apiKey: mailgunApiKey, domain: mailgunDomain });
 const mail = mailgunJSClient.messages();
 
-const sendEmail = async (from, to, text, subject) =>
-  new Promise(resolve => {
+const sendEmail = async (from, to, text, subject) => {
+  let html = `<p>${text}</p>`;
+  html += "<br />";
+  html += "<small>This message was sent you from https://githubprofilesummary.com</small>";
+  return new Promise(resolve => {
     const data = {
       from,
       to,
-      text,
+      html,
       subject
     };
 
     mail.send(data, (error, body) => {
       if (error) {
-        resolve({ error: error.message })
-      }
-      else {
-        resolve(body)
+        resolve({ error: error.message });
+      } else {
+        resolve(body);
       }
     });
   });
-
+};
 
 module.exports = sendEmail;
