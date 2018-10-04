04a48d048894d4fae2bc1b68ffe7c25f8bb0679b
Lourdes Roashan <root.roashan@gmail.com>
2018-10-04 20:18:54 -0300

    Updated report request body

--- src/controllers/validators/report/index.js
+++ src/controllers/validators/report/index.js
@@ -4,6 +4,10 @@ const postUserData = celebrate({
   params: {
     userName: Joi.string().required(),
   },
+  body: {
+    languageChart: Joi.string().dataURI().required(),
+    commitChart: Joi.string().dataURI().required()
+  }
 });
 
 module.exports.postUserData = postUserData;
