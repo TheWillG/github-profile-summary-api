bac785c3489141db69da5ed79995a0c7aeb3794f
Lourdes Roashan <root.roashan@gmail.com>
2018-10-05 12:44:16 -0300

    Added Whitelist for Cors (#47)

--- src/app.js
+++ src/app.js
@@ -4,10 +4,23 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const controllers = require("./controllers");
 const { errors } = require("celebrate");
+const { whitelist, isProduction } = require("../lib/config");
+
+var corsOptions = {
+    origin: function(origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
 
 const app = express();
 app.use(helmet());
-app.use(cors());
+if (isProduction) {
+    app.use(cors(corsOptions));
+}
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use("/api/v1/", controllers);
